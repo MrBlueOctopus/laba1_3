f23fb139b98eba29c8c3e634654e373cc0f3e716
Lizzzard <70705348+MrBlueOctopus@users.noreply.github.com>
2020-09-09 22:25:54 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,11 +6,11 @@ public class Main {
 	System.out.println("Гармонический ряд:");
 	double sum=0;
 	for (int i=1; i<=10; ++i){
-	    System.out.print((double) 1/i + " ");
-	    sum+=(double)1/i;
+	    System.out.print((float) 1/i + " ");
+	    sum+=(float)1/i;
     }
         System.out.println();
         System.out.println("Сумма гармонического ряда:");
-        System.out.print((double)sum);
+        System.out.print((float)sum);
     }
 }
